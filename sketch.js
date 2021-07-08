e9e77a3898a1f8ccb3cc8dda676f5febb187a046
1233-dot <65727973+1233-dot@users.noreply.github.com>
2021-07-08 14:07:55 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,20 @@
+var spriteName
 
 function preload(){
-
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png", "ship-2.png", "ship-3.png", "ship-4.png");
 }
 
 function setup(){
   createCanvas(400,400);
-  
+  preload()
 }
 
 function draw() {
   background("blue");
- 
+  drawSprites();
+
+  spriteName.addImage(seaImg);
+  seaImg.velocityX = 2
 }
+
